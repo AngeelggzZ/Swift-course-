650b93ce9c7355d45b5fd99d5ccae88156595ea0
Angeelggz <dxp7cmwvdc@privaterelay.appleid.com>
2026-01-15 10:04:01 -0600

    Initial Commit

---
+++ IosPracticeApp/IosPracticeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosPracticeAppApp.swift
+//  IosPracticeApp
+//
+//  Created by angel david  on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct IosPracticeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
